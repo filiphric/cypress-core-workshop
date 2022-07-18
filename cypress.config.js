ec67a8d3ed6d516b6a7cc1be4b4ba4f7412bd1c7
Filip Hric <filip@filiphric.com>
2022-07-18 08:32:01 +0200

    improve package scripts and config

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,10 @@
 const { defineConfig } = require('cypress')
-const { setupDb } = require('./workshop-scripts/testSetupTask.js')
+const { setupDb } = require('./workshop-scripts/setupDbFile.js')
 
 module.exports = defineConfig({
   viewportHeight: 550,
   viewportWidth: 660,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       on('task', { setupDb })
     },
