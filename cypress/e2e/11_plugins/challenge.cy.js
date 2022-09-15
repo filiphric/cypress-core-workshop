621552c79704d6792e2029b14c64fce863844f44
Filip Hric <filip@filiphric.com>
2022-09-15 11:58:11 +0200

    fix typo

--- cypress/e2e/11_plugins/challenge.cy.js
+++ cypress/e2e/11_plugins/challenge.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 // challenge #1: install the cypress-real-events plugin
-// which you can use to trigger actions like hover, swipe, etc
+// which you can use to trigger actions like hover, swipe, etc.
 // using this plugin, write a test where you will hover over
 // board item in board list and click on the star element
 it('bookmarks a board', () => {
