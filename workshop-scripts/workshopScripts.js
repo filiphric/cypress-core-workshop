f532dd28d61cd4544ed5244b09be09788d29ab3d
Filip Hric <filip@filiphric.com>
2023-03-21 11:12:12 +0100

    update submodule

--- workshop-scripts/workshopScripts.js
+++ workshop-scripts/workshopScripts.js
@@ -3,7 +3,6 @@ const { testSetupData } = require('./testSetupData.js')
 
 module.exports.registerWorkshopScripts = (on, config) => {
   config.env.hideXhr = Boolean(process.env.cypress_hideXhr)
-  config.scrollBehavior = false
   on('task', { testSetupData })
   return config
 };
