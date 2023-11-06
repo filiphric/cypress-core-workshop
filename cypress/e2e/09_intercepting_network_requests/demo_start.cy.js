5b796f58cdd9e17901a9e807b5556ac48e89c0e7
Filip Hric <filip@filiphric.sk>
2023-11-06 10:27:00 +0100

    add examples to network requests

--- cypress/e2e/09_intercepting_network_requests/demo_start.cy.js
+++ cypress/e2e/09_intercepting_network_requests/demo_start.cy.js
@@ -20,3 +20,33 @@ it('deleting a list', () => {
     .click()
 
 });
+
+it('loads a list of boards from fixture', () => {
+
+  cy.intercept({
+    method: 'GET', 
+    url: '/api/boards'
+  })
+    .as('boardList')
+
+  cy.visit('/');
+
+})
+
+it('shows an error message when creating a board', () => {
+
+  cy.intercept({
+    method: 'POST', 
+    url: '/api/boards'
+  })
+    .as('boardCreate')
+
+  cy.visit('/');
+
+  cy.get('[data-cy=create-board]')
+    .click()
+
+  cy.get('[data-cy=new-board-input]')
+    .type('garden project{enter}')
+
+})
